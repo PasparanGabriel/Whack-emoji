be4d9da4071494b262b2adde1d0fb0f390ea6968
PasparanGabriel <pasparangabriel@yahoo.com>
2019-11-08 22:41:27 +0000

    Add button for start game

--- js/main.js
+++ js/main.js
@@ -1,30 +1,39 @@
-var score = 0;
-var randomNumber;
-var gameOver = false;
+var gameOver
+var interval
+var randomNumber
+var score
 
-var interval = setInterval(faceJump, 1000);
+function start() {
+  interval = setInterval(faceJump, 1000)
+  gameOver = false
+  score = 0
 
-$("li").click(faceClick);
+  $('.result').html('Your Score: 0')
+  $(".start").attr("hidden", true)
+  $("li").click(faceClick)
+}
 
 function faceJump() {
-  $('li').removeClass('emoji');
+  $('li').removeClass('emoji')
   
-  randomNumber = Math.floor(Math.random() * 9);
-  $('li').eq(randomNumber).addClass('emoji');
+  randomNumber = Math.floor(Math.random() * 9)
+  $('li').eq(randomNumber).addClass('emoji')
 }
 
 function faceClick() {
   if ($(this).hasClass('emoji') && !gameOver) {
-    score++;
-    $('#result').html('Your Score: ' + score);
+    score++
+    $('.result').html('Your Score: ' + score)
   }
-  else {  
-    clearInterval(interval);
-    gameOver = true;
+  else {
+    gameOver = true
   }
 
   if (gameOver) {
-    $('#result').html('Game Over<br>' + 'Your Score: ' + score);   
-    $('li').click(false);
+    $('.result').html('Game Over<br>' + 'Your Score: ' + score)
+    $('li').click(false)
+    $(".start").removeAttr("hidden")
+
+    clearInterval(interval)
   }
 }
