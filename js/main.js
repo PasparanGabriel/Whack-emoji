eae06c251b589a30cc0154f8031cb791e29bc297
PasparanGabriel <pasparangabriel@yahoo.com>
2019-11-09 11:16:43 +0000

    Change cursor at click

--- js/main.js
+++ js/main.js
@@ -9,15 +9,15 @@ function start() {
   score = 0
 
   $('.result').html('Your Score: 0')
-  $(".start").attr("hidden", true)
-  $("li").click(faceClick)
+  $('.start').attr('hidden', true)
+  $('.item').click(faceClick)
 }
 
 function faceJump() {
-  $('li').removeClass('emoji')
+  $('.item').removeClass('emoji')
   
   randomNumber = Math.floor(Math.random() * 9)
-  $('li').eq(randomNumber).addClass('emoji')
+  $('.item').eq(randomNumber).addClass('emoji')
 }
 
 function faceClick() {
@@ -31,9 +31,20 @@ function faceClick() {
 
   if (gameOver) {
     $('.result').html('Game Over<br>' + 'Your Score: ' + score)
-    $('li').click(false)
-    $(".start").removeAttr("hidden")
+    $('.item').click(false)
+    $('.start').removeAttr('hidden')
 
     clearInterval(interval)
   }
 }
+
+$(document).ready(function() {
+  $('.item').click(function() {
+    var intervalCursor = setInterval(() => {
+      $(this).css('cursor', 'url(img/cursor.cur), auto')
+      clearInterval(intervalCursor)
+    }, 1000)
+
+    $(this).css('cursor', 'url(img/cursorClick.cur), auto')
+  })
+})
