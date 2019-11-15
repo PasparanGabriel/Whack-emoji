e68a44a01d0b509490969f19780ee09a8a5e03ff
PasparanGabriel <pasparangabriel@yahoo.com>
2019-11-15 15:04:48 +0000

    Add timer

--- js/main.js
+++ js/main.js
@@ -4,18 +4,27 @@ var gameOver
 var interval
 var randomNumber
 var score
+var timer
 
 function start() {
   audioWin = new Audio('audio/win.mp3')
   audioGameOver = new Audio('audio/gameOver.mp3')
   interval = setInterval(faceJump, 1200)
   gameOver = false
+  minutes = 2
+  seconds = 0
   score = 0
+  timer = setInterval(timerOn, 1000)
 
-  $('.result').html('Your Score: 0')
-  $('.gameOver').css('visibility', 'hidden')
-  $('.start').attr('disabled', 'disabled')
+  $('.gameOver').css('visibility', 'visible')
+  $('.gameOver').html(
+    (minutes < 10 ? '0' + minutes : minutes)
+    + ':' +
+    (seconds < 10 ? '0' + seconds : seconds)
+  )
   $('.item').click(faceClick)
+  $('.result').html('Your Score: 0')
+  $('.start').attr('disabled', 'disabled')
 }
 
 function faceJump() {
@@ -34,15 +43,41 @@ function faceClick() {
   }
   else {
     gameOver = true
-    audioGameOver.play()
   }
+}
 
+function timerOn() {
   if (gameOver) {
-    $('.gameOver').css('visibility', 'visible')
-    $('.item').click(false)
-    $('.start').removeAttr('disabled')
+    audioGameOver.play()
 
     clearInterval(interval)
+    clearInterval(timer)
+
+    $('.gameOver').css('color', 'white')
+    $('.gameOver').html('Game Over')
+    $('.item').click(false)
+    $('.start').removeAttr('disabled')
+  } else {
+    if (seconds === 0) {
+      if (minutes === 0) {
+        gameOver = true
+      } else {
+        minutes--
+        seconds = 59
+      }
+    } else {
+      seconds--
+
+      if (minutes === 0 && seconds < 4) {
+        $('.gameOver').css('color', 'red')
+      }
+    }
+  
+    $('.gameOver').html(
+      (minutes < 10 ? '0' + minutes : minutes)
+      + ':' +
+      (seconds < 10 ? '0' + seconds : seconds)
+    )
   }
 }
 
